fe3eafc7d8f4e4af9273d525b50736464d14cbcc
pillai.ashwini@gmail.com <pillai.ashwini@gmail.com>
2018-07-19 03:15:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
   delete newObj[key];
-  return 
+  return newObj;
 }
